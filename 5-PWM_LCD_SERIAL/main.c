1423bc74b6f1e38787a36c44a5118e0bcd2d5763
Kyra Server <kyraserver@localhost>
2023-08-22 00:30:02 -0300

    Auto commit - 22 Aug 00:30

--- 5-PWM_LCD_SERIAL/main.c
+++ 5-PWM_LCD_SERIAL/main.c
@@ -19,7 +19,7 @@
 #include "queue.h"
 #include "task.h"
 
-// #include <string.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define __delay_ms( TEMPO ) vTaskDelay(pdMS_TO_TICKS( TEMPO ))
@@ -30,19 +30,19 @@
 
 // LCD 16x2
 #include "myLcd.h"
-// #include "Serial.h"
+#include "Serial.h"
 #include "PWM.h"
-
+#include "dht11.h"
 
 #define BIN 2
 #define OCT 8
 #define DEC 10
 #define HEX 16
 
-static QueueHandle_t fila_ADC; 
-static QueueHandle_t LCD_Flag_queue; 
+static QueueHandle_t LCD_DHT_queue; 
 
 static void task1(void *args __attribute((unused))) {
+
 	/* [2---]
 	 *
 	 * Tarefa dedicada em mostrar informações no LCD 16x2.
@@ -60,17 +60,18 @@ static void task1(void *args __attribute((unused))) {
 	set_bit(IO,D7);   disp_init();
 
   disp_cmd(0x0C);
+/*============================================================*/
 
-  disp_text("Teste De\\ !&(#@!%@",0,0);
-  u8 cont = 0;
+  disp_text("Temperatura: ",0,0);
+
+  char msg_buf_rev[16];
 	while(1){
+		//
+		// testar para ver se recebe !!!
+		__delay_ms(1000);
+		xQueueReceive(LCD_DHT_queue, &msg_buf_rev, pdMS_TO_TICKS(1));
 
-		cont ++;
-		cont &= 0x00ff;
-		disp_text("CONTADOR: ", 1, 0);
-		disp_number( cont, 1, 11);
-		__delay_ms(10);
-		
+		disp_text(msg_buf_rev, 1, 0);
 	}
 }
 
@@ -84,7 +85,7 @@ static void task2(void *args __attribute ((unused))){
 	}
 }
 
-static void task3(void *args __attribute ((unused))){
+ void task3(void *args __attribute ((unused))){
 	/* [1---]
 	 *
 	 * Tarefa dedicada a leitura do sensor de temperatura e
@@ -93,10 +94,49 @@ static void task3(void *args __attribute ((unused))){
 	 * O valor amostrado deve ser transmitido a cada 500ms
 	 * usando área de memória compartilhada. -> [2---]
 	 *
+	 * Problemas: A configuração do TIMER está errada nas
+	 * linhas: 15 e 93 do arquivo dht11.c !!!!!
+	 *
+	 * Solução: 
+	 *		Comparar com a biblioteca funcional do outro
+	 *		projeto!!!
+	 *
+	 *
+	 *
 	 * */
 
+	struct DHT11_Dev dev;
+	DHT11_init(&dev, GPIOB, GPIO_Pin_6);
+
+	char msg_buf_env[16];
+	char  Humidade[4];
+	char 	Temperatura[4];
+
 	while(1){
+		__delay_ms(500); // << Rever isso !!
+
+		int leitura_DHT = DHT11_read( &dev );
+
+		if(leitura_DHT == DHT11_SUCCESS) {
+			
+			// Converte em string 
+			itoa(dev.temparature, Temperatura , DEC);
+			itoa(dev.humidity, Humidade, DEC);
+
+			// Concatena  a variável "msg_buf_env"
+			strcpy(msg_buf_env, "T: ");
+			strcat(msg_buf_env, Temperatura );
+			strcat(msg_buf_env, "H: " );
+			strcat(msg_buf_env, Humidade );
+		}
+
+		else if(leitura_DHT == DHT11_ERROR_CHECKSUM ) 
+			strcpy(msg_buf_env, "ERR_checksum"); 
 		
+		else 
+			strcpy(msg_buf_env, "TIMOUT"); 
+
+		xQueueSend(LCD_DHT_queue, &msg_buf_env, 0);
 	}
 }
 
@@ -105,30 +145,27 @@ static void task4(void *args __attribute ((unused))){
 	 * Tarefa dedicada a leitura de botões para controlar o
 	 * PWM -> ajuste de contraste.
 	 * */
+	
 	while(1){
 		
 	}
 }
 
-
 int main(void) {
-	fila_ADC = xQueueCreate(5 /* Tamanho máximo da fila. */,
-								sizeof(u32) /* Largura máxima da fila. */);
-	LCD_Flag_queue = xQueueCreate(2, sizeof(u8));
+
+	LCD_DHT_queue = xQueueCreate(2, sizeof(char));
 	
 	set_system_clock_to_72Mhz();
 
 	xTaskCreate(task1,"LCD_16x2", 100 ,NULL, 4 ,NULL);
 	xTaskCreate(task2, "LED_13", 100 , NULL, 4 , NULL);
-	xTaskCreate(task3, "USArt", 100 , NULL, 4 , NULL);
+	xTaskCreate(task3, "DHT_read", 100 , NULL, 4 , NULL);
 	xTaskCreate(task4, "Contrast", 100 , NULL, 4 , NULL);
 
-	vTaskStartScheduler(); // inicia o escalonador de
-								  // processos.
+	vTaskStartScheduler(); 
+
 	while(1);
 	return 0;
 }
 
-/*==========================================================*/
-/*==========================================================*/
 /*==========================================================*/
